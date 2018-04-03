bc6005b39ce342fd7c24c440a0d89f255ed3078f
Blake Boswell <blake.w.boswell@gmail.com>
2018-04-03 02:44:49 -0500

    RdotV and LdotN set up

--- PhongShading.cpp
+++ PhongShading.cpp
@@ -1,11 +1,11 @@
 #include "PhongShading.h"
 
 PhongShading::PhongShading() {
-    this->cameraPosition = NULL;
-    this->lightSourceColor = NULL;
-    this->lightSourcePosition = NULL;
-    this->objectColor = NULL;
-    this->materialProperties = NULL;
+    this->cameraPosition = new float[3]{0.0, 0.0, 0.0};
+    this->lightSourceColor = new int[3]{0, 0, 0};
+    this->lightSourcePosition = new float[3]{0.0, 0.0, 0.0};
+    this->objectColor = new int[3]{0, 0, 0};
+    this->materialProperties = new float[4]{0.0, 0.0, 0.0, 0.0};
 }
 
 PhongShading::~PhongShading() {
@@ -37,6 +37,66 @@ void PhongShading::setObject(int* objectColor, float* materialProperties) {
     this->materialProperties[3] = materialProperties[3];
 }
 
-int* PhongShading::getShade(int* pixelLocation, int* surfaceNormal) {
+float* PhongShading::getShade(float* pixelLocation, float* surfaceNormal) {
+    int* shade = new int[3];
+
+    // Calculate ambient color values
+    // ambient absorption coefficient (Ka) * Color (R,G,B) = Ia vector
+    float R = this->materialProperties[0] * this->objectColor[0];
+    float G = this->materialProperties[0] * this->objectColor[1];
+    float B = this->materialProperties[0] * this->objectColor[2];
+
+    // Generate LdotN vector
+    float* lightVector = new float[3];
+    float* normalVector = new float[3];
+
+    lightVector = normalize(this->lightSourcePosition);
+    normalVector = normalize(surfaceNormal);
+
+    float LdotN = dot(lightVector, normalVector);
+    if(LdotN < 0.0) {
+        LdotN = 0.0;
+    }
+
+    // Generate RdotV vector
+    float* reflectionVector = new float[3];
+    float* viewVector = new float[3];
+
+    for(int i = 0; i < 3; i++) {
+        reflectionVector[i] = 2 * LdotN * pixelLocation[i] - this->lightSourcePosition[i];
+    }
+    reflectionVector = normalize(reflectionVector);
+    viewVector = normalize(this->cameraPosition);
+
+    
+    float RdotV = dot(reflectionVector, viewVector);
+    if(RdotV < 0.0) {
+        RdotV = 0.0;
+    }
+
+
+}
+
+float PhongShading::dot(float a[], float b[], int vectorSize = 3) {
+    float dotResult = 0.0;
+    for(int i = 0; i < vectorSize; i++) {
+        dotResult += a[i] * b[i];
+    }
+    return dotResult;
+}
+
+float PhongShading::dot(int a[], int b[], int vectorSize = 3) {
+    float dotResult = 0.0;
+    for(int i = 0; i < vectorSize; i++) {
+        dotResult += a[i] * b[i];
+    }
+    return dotResult;
+}
+
+float* PhongShading::normalize(float* vector) {
+
+}
+
+float* PhongShading::normalize(int* vector) {
 
 }
--- PhongShading.h
+++ PhongShading.h
@@ -8,9 +8,25 @@ class PhongShading {
     private:
     static float* cameraPosition;
     static int* lightSourceColor;
-    static float* lightSourcePosition;
+    static float* lightSourcePosition;  
     static int* objectColor;
-    static float* materialProperties;
+    static float* materialProperties;   // Ka, Kd, Ks, alpha
+    float dot(float a[], float b[], int vectorSize = 3);
+    float dot(int a[], int b[], int vectorSize = 3);
+    float* normalize(float* vector);
+    float* normalize(int* vector);
+    // ambient absorption coefficient (Ka)
+    // diffuse absorption coefficient (Kd)
+    // specular absorption coefficient (Ks)
+    // shininess coefficient (alpha)
+    // light source ambient (Ia)
+    // light source diffuse (Id)
+    // light source specular (Is)
+    // normal vector (N)
+    // light vector (L)
+    // view vector (V)
+    // reflection vector (R)
+    // Ip = Ka * Ia + Kd * (^L dot ^N) * Id + Ks * (^R dot ^V)^alpha * Is
 
     public:
     PhongShading();
@@ -18,5 +34,5 @@ class PhongShading {
     void setCamera(int* cameraPosition);
     void setLight(int* lightSourceColor, float* lightSourcePosition);
     void setObject(int* objectColor, float* materialProperties);
-    int* getShade(int* pixelLocation, int* surfaceNormal);
+    float* getShade(float* pixelLocation, float* surfaceNormal);
 }
