1e79c4cda3677d554f11e30fc3399044e1f8b8c3
Blake Boswell <blake.w.boswell@gmail.com>
2018-04-05 21:15:40 -0500

    Somewhat working shading

--- PhongShading.cpp
+++ PhongShading.cpp
@@ -44,31 +44,31 @@ float* PhongShading::getShade(float* pixelLocation, float* surfaceNormal) {
     float Kd = PhongShading::materialProperties[1];
     float Ks = PhongShading::materialProperties[2];
     float alpha = PhongShading::materialProperties[3];
-    int IaRed = PhongShading::objectColor[0];
-    int IaGreen = PhongShading::objectColor[1];
-    int IaBlue = PhongShading::objectColor[2];
-    int IdRed = PhongShading::lightSourceColor[0] * (objectColor[0]/255.0);
-    int IdGreen = PhongShading::lightSourceColor[1] * (objectColor[1]/255.0);
-    int IdBlue = PhongShading::lightSourceColor[2] * (objectColor[2]/255.0);
-    int IsRed = PhongShading::lightSourceColor[0];
-    int IsGreen = PhongShading::lightSourceColor[1];
-    int IsBlue = PhongShading::lightSourceColor[2];
+    float IaRed = (PhongShading::objectColor[0]) * (PhongShading::lightSourceColor[0]/255.0);
+    float IaGreen = (PhongShading::objectColor[1]) * (PhongShading::lightSourceColor[1]/255.0);
+    float IaBlue = (PhongShading::objectColor[2]) * (PhongShading::lightSourceColor[2]/255.0);
+    float IdRed = (PhongShading::lightSourceColor[0]) * (objectColor[0]/255.0);
+    float IdGreen = (PhongShading::lightSourceColor[1]) * (objectColor[1]/255.0);
+    float IdBlue = (PhongShading::lightSourceColor[2]) * (objectColor[2]/255.0);
+    float IsRed = PhongShading::lightSourceColor[0] * (objectColor[0]/255.0);
+    float IsGreen = PhongShading::lightSourceColor[1] * (objectColor[1]/255.0);
+    float IsBlue = PhongShading::lightSourceColor[2] * (objectColor[2]/255.0);
 
-    int ambientRed = 0;
-    int ambientGreen = 0;
-    int ambientBlue = 0;
-    int diffuseRed = 0;
-    int diffuseGreen = 0;
-    int diffuseBlue = 0;
-    int specularRed = 0;
-    int specularGreen = 0;
-    int specularBlue = 0;
+    float ambientRed = 0;
+    float ambientGreen = 0;
+    float ambientBlue = 0;
+    float diffuseRed = 0;
+    float diffuseGreen = 0;
+    float diffuseBlue = 0;
+    float specularRed = 0;
+    float specularGreen = 0;
+    float specularBlue = 0;
 
-    cout << "Camera " << PhongShading::cameraPosition[0] << " " << PhongShading::cameraPosition[1] << " " << PhongShading::cameraPosition[2] << endl;
-    cout << "Light " << PhongShading::lightSourceColor[0] << " " << PhongShading::lightSourceColor[1] << " " << PhongShading::lightSourceColor[2] << " " << PhongShading::lightSourcePosition[0] << " " << PhongShading::lightSourcePosition[1] << " " << PhongShading::lightSourcePosition[2] << endl;
-    cout << "Object " << PhongShading::objectColor[0] << " " << PhongShading::objectColor[1] << " " << PhongShading::objectColor[2] << " " << PhongShading::materialProperties[0] << " " << PhongShading::materialProperties[1] << " " << PhongShading::materialProperties[2] << " " << PhongShading::materialProperties[3] << endl;
-    cout << endl;
-    cout << "Point " << pixelLocation[0] << " " << pixelLocation[1] << " " << pixelLocation[2] << " " << surfaceNormal[0] << " " << surfaceNormal[1] << " " << surfaceNormal[2] << endl;
+    // cout << "Camera " << PhongShading::cameraPosition[0] << " " << PhongShading::cameraPosition[1] << " " << PhongShading::cameraPosition[2] << endl;
+    // cout << "Light " << PhongShading::lightSourceColor[0] << " " << PhongShading::lightSourceColor[1] << " " << PhongShading::lightSourceColor[2] << " " << PhongShading::lightSourcePosition[0] << " " << PhongShading::lightSourcePosition[1] << " " << PhongShading::lightSourcePosition[2] << endl;
+    // cout << "Object " << PhongShading::objectColor[0] << " " << PhongShading::objectColor[1] << " " << PhongShading::objectColor[2] << " " << PhongShading::materialProperties[0] << " " << PhongShading::materialProperties[1] << " " << PhongShading::materialProperties[2] << " " << PhongShading::materialProperties[3] << endl;
+    // cout << endl;
+    // cout << "Point " << pixelLocation[0] << " " << pixelLocation[1] << " " << pixelLocation[2] << " " << surfaceNormal[0] << " " << surfaceNormal[1] << " " << surfaceNormal[2] << endl;
     float* shade = new float[3];
 
     // Calculate light ambient color
@@ -81,36 +81,38 @@ float* PhongShading::getShade(float* pixelLocation, float* surfaceNormal) {
     float* normalizedLightVector = new float[3];
     float* normalizedNormalVector = new float[3];
 
+    normalizedNormalVector = normalize(surfaceNormal);
     normalizedLightVector = normalize(PhongShading::lightSourcePosition);
+    //normalizedLightVector = PhongShading::lightSourcePosition;
 
-    float LdotN = dot(normalizedLightVector, surfaceNormal);
+    float LdotN = dot(normalizedLightVector, normalizedNormalVector);
 
-    for(int i = 0; i < 3; i++) {
-        if(i == 0) cout << "X" << endl;
-        else if (i == 1) cout << "Y" << endl;
-        else cout << "Z" << endl;
-        cout << "\tLight Vector: " << PhongShading::lightSourcePosition[i] << endl;
-        cout << "\tLight Vector Magnitude: " << PhongShading::lightSourcePosition[i]/normalizedLightVector[i] << endl;
-        cout << "\tNormalized Light Vector: " << normalizedLightVector[i] << endl;
-        cout << "\tNormal Vector: " << surfaceNormal[i] << endl;
-        cout << "\tNormal Vector Magnitude: " << surfaceNormal[i]/surfaceNormal[i] << endl;
-        // cout << "\tNormalized Normal Vector: " << surfaceNormal[i] << endl;
-    }
-    cout << "L dot N: " << LdotN << endl;
-    cout << "Normalized Light Vector Magnitude: " << magnitude(normalizedLightVector) << endl;
-    cout << "Normalized Normal Vector Magnitude: " << magnitude(surfaceNormal) << endl;
+    // for(int i = 0; i < 3; i++) {
+    //     if(i == 0) cout << "X" << endl;
+    //     else if (i == 1) cout << "Y" << endl;
+    //     else cout << "Z" << endl;
+    //     cout << "\tLight Vector: " << PhongShading::lightSourcePosition[i] << endl;
+    //     cout << "\tLight Vector Magnitude: " << magnitude(PhongShading::lightSourcePosition) << endl;
+    //     cout << "\tNormalized Light Vector: " << normalizedLightVector[i] << endl;
+    //     cout << "\tNormal Vector: " << surfaceNormal[i] << endl;
+    //     cout << "\tNormal Vector Magnitude: " << magnitude(surfaceNormal) << endl;
+    //     cout << "\tNormalized Normal Vector: " << normalizedNormalVector[i] << endl;
+    // }
+    // cout << "L dot N: " << LdotN << endl;
+    // cout << "Normalized Light Vector Magnitude: " << magnitude(normalizedLightVector) << endl;
+    // cout << "Normalized Normal Vector Magnitude: " << magnitude(normalizedNormalVector) << endl;
 
     if(LdotN > 0.0) {
-        cout << "LdotN" << endl;
+        // cout << "LdotN" << endl;
         // Calculate diffuse value
         diffuseRed = Kd * LdotN * IdRed;
         diffuseGreen = Kd * LdotN * IdGreen;
         diffuseBlue = Kd * LdotN * IdBlue;
 
-        cout << PhongShading::lightSourceColor[0] << " x (" << objectColor[0] << "/255) = " << IdRed << endl; 
-        cout << "Id\nR: " << IdRed << "\nG: " << IdGreen << "\nB: " << IdBlue << endl;
-        cout << "Diffuse\nR: " << diffuseRed << "\nG: " << diffuseGreen << "\nB: " << diffuseBlue << endl;
-        cout << "Ambient\nR: " << ambientRed << "\nG: " << ambientGreen << "\nB: " << ambientBlue << endl;
+        // cout << PhongShading::lightSourceColor[0] << " x (" << objectColor[0] << "/255) = " << IdRed << endl; 
+        // cout << "Id\nR: " << IdRed << "\nG: " << IdGreen << "\nB: " << IdBlue << endl;
+        // cout << "Diffuse\tR: " << diffuseRed << "\tG: " << diffuseGreen << "\tB: " << diffuseBlue << endl;
+        // cout << "Ambient\tR: " << ambientRed << "\tG: " << ambientGreen << "\tB: " << ambientBlue << endl;
         
         // Generate RdotV vector
         float* reflectionVector = new float[3];
@@ -119,15 +121,22 @@ float* PhongShading::getShade(float* pixelLocation, float* surfaceNormal) {
         // Ideal reflector vector PPT 18
         for(int i = 0; i < 3; i++) {
 
-            reflectionVector[i] = 2 * LdotN * pixelLocation[i] - PhongShading::lightSourcePosition[i];
+            reflectionVector[i] = 2 * LdotN * (normalizedNormalVector[i] - normalizedLightVector[i]);
         }
         reflectionVector = normalize(reflectionVector);
         viewVector = normalize(PhongShading::cameraPosition);
+        // cameraX - x
+        // cameraY - y
+        // cameraZ - z
+
+        // cout << "Reflection vector: (" << reflectionVector[0] << ", " << reflectionVector[1] << ", " << reflectionVector[3] << ")\n\tMag: " << magnitude(reflectionVector) << endl;
+        // cout << "View vector: (" << viewVector[0] << ", " << viewVector[1] << ", " << viewVector[2] << ")\n\tMag: " << magnitude(viewVector) << endl;
 
         
         float RdotV = dot(reflectionVector, viewVector);
+        // cout << "R dot V: " << RdotV << endl;
         if(RdotV > 0.0) {
-            cout << "RdotV" << endl;
+            // cout << "RdotV" << endl;
             // Calculate light specular value
             float Ks = PhongShading::materialProperties[2];
             RdotV = pow(RdotV, PhongShading::materialProperties[3]);
@@ -140,9 +149,9 @@ float* PhongShading::getShade(float* pixelLocation, float* surfaceNormal) {
         }
     }
 
-    shade[0] = ambientRed + diffuseRed + specularRed;
-    shade[1] = ambientGreen + diffuseGreen + specularGreen;
-    shade[2] = ambientBlue + diffuseBlue + specularBlue;
+    shade[0] = (ambientRed + diffuseRed + specularRed);
+    shade[1] = (ambientGreen + diffuseGreen + specularGreen);
+    shade[2] = (ambientBlue + diffuseBlue + specularBlue);
 
     return shade;
 
--- main.cpp
+++ main.cpp
@@ -3,27 +3,114 @@
 
 #include <iostream>
 #include <stdlib.h>
+#include <fstream>
+#include <string>
+#include <vector>
 #include "PhongShading.h"
 
 using namespace std;
 
-int main() {
-    PhongShading* p = new PhongShading();
+vector<string> split(string text, string delimiter) {
+    vector<string> list;
+    size_t pos = 0;
+    string token;
+    while((pos = text.find(delimiter)) != -1) {
+        token = text.substr(0, pos);
+        list.push_back(token);
+        text.erase(0, pos + delimiter.length());
+    }
+    token = text.substr(0, text.length());
+    list.push_back(token);
+    return list;
+}
+
+enum stringCode {
+    Camera,
+    Light,
+    Object,
+    Point
+};
+
+stringCode hashString(string const& input) {
+    if(input == "Camera") {
+        return Camera;
+    }
+    if(input == "Light") {
+        return Light;
+    }
+    if(input == "Object") {
+        return Object;
+    }
+    if(input == "Point") {
+        return Point;
+    }
+}
+
+void readFile(string filename) {
     float* cameraPosition = new float[3]{0.0, 0.0, -300.0};
     int* lightSourceColor = new int[3]{100, 100, 100};
     float* lightSourcePosition = new float[3]{.5, .5, -.7};
     int* objectColor = new int[3]{255, 255, 0};
     float* materialProperties = new float[4]{0.2, 0.4, 0.4, 2.0};
-    p->setCamera(cameraPosition);
-    p->setLight(lightSourceColor, lightSourcePosition);
-    p->setObject(objectColor, materialProperties);
-
     float* pixelLocation = new float[3]{100.0, 100.0, 200.0};
     float* surfaceNormal = new float[3]{0.398925, 0.598388, -0.453324};
-    float* shade = p->getShade(pixelLocation, surfaceNormal);
-    cout << "Shade Red: " << shade[0] << endl;
-    cout << "Shade Green: " << shade[1] << endl;
-    cout << "Shade Blue: " << shade[2] << endl;
+    PhongShading* p = new PhongShading();
+    vector<string> words;
+    fstream fs;
+    string line;
+    string delimiter = " ";
+    stringCode type;
+
+    fs.open(filename.c_str(), fstream::in);
+    if(fs.is_open()) {
+        while(!fs.eof()) {
+            getline(fs, line);
+            if(line.length() > 0) {
+                words = split(line, delimiter);
+                // for(int i = 0; i < words.size(); i++) {
+                //     cout << "[" << i << "] " << words[i] << endl;
+                // }
+                type = hashString(words[0].c_str());
+                switch(type) {
+                    case Camera:
+                        for(int i = 0; i < 3; i++) {
+                            cameraPosition[i] = atof(words[i+1].c_str());
+                        }
+                        PhongShading::setCamera(cameraPosition);
+                        break;
+                    case Light:
+                        for(int i = 0; i < 3; i++) {
+                            lightSourceColor[i] = atoi(words[i+1].c_str());
+                            lightSourcePosition[i] = atof(words[i+4].c_str());
+                        }
+                        PhongShading::setLight(lightSourceColor, lightSourcePosition);
+                        break;
+                    case Object:
+                        for(int i = 0; i < 4; i++) {
+                            if(i < 3) {
+                                objectColor[i] = atoi(words[i+1].c_str());
+                            }
+                            materialProperties[i] = atof(words[i+4].c_str());
+                        }
+                        PhongShading::setObject(objectColor, materialProperties);
+                        break;
+                    case Point: 
+                        for(int i = 0; i < 3; i++) {
+                            pixelLocation[i] = atof(words[i+1].c_str());
+                            surfaceNormal[i] = atof(words[i+4].c_str());
+                        }
+                        float* shade = p->getShade(pixelLocation, surfaceNormal);
+                        cout << pixelLocation[0] << "," << pixelLocation[1] << "," << pixelLocation[2] << " " << shade[0] << "," << shade[1] << "," << shade[2] << endl; 
+                }
+            } 
+        }
+        fs.close();
+    }
+
+}
+
+int main() {
+    readFile("input.txt");
 
     return 0;
 }
